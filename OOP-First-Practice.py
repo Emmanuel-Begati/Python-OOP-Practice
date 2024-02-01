013c4f53405764e78947606fd853a3fa7fe6e50a
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-02-01 18:50:25 +0200

    practicing how to add attributes to classes

--- OOP-First-Practice.py
+++ OOP-First-Practice.py
@@ -1,12 +1,17 @@
 #Instantiate my class
 class Item():
+    def calculate_total_prices(self, x, y):
+        return x * y
     pass
 
 item = Item()
 item.name = 'Laptop'
 item.price = 2000
 item.quantity = 2
+print(item.calculate_total_prices(item.price, item.quantity))
 
-print(item.name)
-print(item.price)
-print(item.quantity)
+item2 = Item()
+item2.name = 'Phone'
+item2.price = 200
+item2.quantity = 3
+print(item2.calculate_total_prices(item2.price, item2.quantity))
