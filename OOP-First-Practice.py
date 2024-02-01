c8a5aacda4920b6d27fd1b4f9775e3345f955ee8
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-02-01 22:08:31 +0200

    added assertation

--- OOP-First-Practice.py
+++ OOP-First-Practice.py
@@ -1,19 +1,28 @@
 #Instantiate my class
 class Item:
-    def __init__(self, name, price, quantity):
-        print(f'Instance has been created for: {name}')
+
+    def __init__(self, name: str, price: float, quantity=0):
+        #Validate my code to only accept from the correc interegers
+        assert quantity >= 0
+        assert price >= 0,  f"The Price: {price}, is less than 1"
+        assert quantity >= 0, f"The Quantity: {quantity}, is less than 1"
+
+
+
         self.name = name
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_prices(self, x, y):
-        return x * y
-    pass
+    def calculate_total_prices(self):
+        return self.price * self.quantity
+    
 
-item1 = Item('Laptop', 2000, 2)
+item1 = Item('Laptop', -2000, 2)
 
-item2 = Item('Phone', 200, 3)
+item2 = Item('Phone', -200, 3)
+
+item2.has_numpad = False #Meaning you can add methods to classes even after.
 
 
-print (item1.name)
-print (item2.name)
+print (item1.calculate_total_prices())
+print (item2.calculate_total_prices())
