aafa37c73974402dc50100ad91806d8a4acb5896
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-02-01 18:24:23 +0200

    this is getting interesting

---
+++ OOP-First-Practice.py
@@ -0,0 +1,12 @@
+#Instantiate my class
+class Item():
+    pass
+
+item = Item()
+item.name = 'Laptop'
+item.price = 2000
+item.quantity = 2
+
+print(item.name)
+print(item.price)
+print(item.quantity)
